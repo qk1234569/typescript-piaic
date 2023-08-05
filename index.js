ecc5587882a8c2b0cb6d886e89992487a90acfc3
qk1234569 <141304095+qk1234569@users.noreply.github.com>
2023-08-05 09:05:23 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+"use strict";
+console.log("hello my name is qaim raza khan");
+// string
+let firstName = "qaim";
+// number 
+let score = 25;
+// boolean
+let isMarried = false;
+// null
+let abc = null;
+// undefined
+let lastName;
+// console.log(lastName)
+// console.log("hello"+firstName);
+console.log(`hello my name is qaim raza khan  ${firstName} `);
+// type of 
+// console.log(typeof firstName);
+// arithmetic operators 
+// sum 
+let n1 = 5;
+let n2 = 10;
+console.log(n1 / n2);
+// subtract
+// n1-n2
+// multiply 
+// n1*n2
+// division
+// n1/n2
+// comparison operators
+let n = 5;
+console.log(n == 5); //true
+console.log(n === 5); //true
+console.log(n != 5); //false
+console.log(n > 8); //false
+console.log(n < 8); //false
+console.log(n >= 8); //false
+console.log(n <= 8); //false
+// condition
+// if else
+n = 6;
+if (n == 5) {
+    console.log('if condition');
+}
+else {
+    console.log("else part");
+}
+// if else if condition
+n = 6;
+if (n == 7) {
+    console.log("if condition");
+}
+else if (n == 5) {
+    console.log("else if conditon");
+}
---
+++ index.ts
@@ -0,0 +1,65 @@
+console.log("hello my name is qaim raza khan");
+
+// string
+let firstName: string="qaim" ;
+
+// number 
+let score=25;
+// boolean
+let isMarried : boolean  = false ;
+// null
+let abc: null = null;
+// undefined
+let lastName;
+
+// console.log(lastName)
+// console.log("hello"+firstName);
+console.log(`hello my name is qaim raza khan  ${firstName} `) 
+
+// type of 
+// console.log(typeof firstName);
+// arithmetic operators 
+// sum 
+let n1=5;
+let n2=10;
+console.log(n1/n2);
+// subtract
+// n1-n2
+// multiply 
+// n1*n2
+// division
+// n1/n2
+// comparison operators
+let n=5;
+console.log(n==5);//true
+console.log(n===5);//true
+console.log(n!=5);//false
+console.log(n>8);//false
+console.log(n<8);//false
+console.log(n>=8);//false
+console.log(n<=8);//false
+// condition
+// if else
+n=6
+if (n==5)  {
+console.log('if condition');}
+else {console.log("else part")}
+// if else if condition
+n=6
+if (n==7) {
+    console.log("if condition")
+}
+else if(n==5){console.log("else if conditon");
+}
+
+
+
+
+
+
+
+
+
+
+
+
